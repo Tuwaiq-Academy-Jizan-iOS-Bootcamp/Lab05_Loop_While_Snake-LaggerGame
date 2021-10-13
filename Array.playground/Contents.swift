c7ef60c5447214a796fa46fbae434a40ac955b94
YOUSEF ALMALKI <91572706+mrdas2030@users.noreply.github.com>
2021-10-13 18:55:16 +0300

    Update Contents.swift

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -1,13 +1,44 @@
 import UIKit
-let randomInt = Int.random(in: 1..<7)
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
 
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
 
-//print(randomInt)
+var square = 0
+var diceRoll = 0
 
-
-var numbers = [1,2,3,4,5,6]
-
-
-if let randomInt = numbers.randomElement() {
-    print("Move to box \(randomInt).")
+// Version with While Loop
+while square < finalSquare {
+    // roll the dice
+    diceRoll = Int.random(in: 1...6)
+    print("The Number of dice is \(diceRoll)")
+    
+    // move by the rolled amount
+    square += diceRoll
+    print("My current square is \(square)")
+    
+    if square < board.count {
+        // if we're still on the board, move up or down for a snake or a ladder
+        square += board[square]
+        print("I move to \(square)")
+    }
 }
+
+// Version with Repeat Loop
+/*repeat {
+    // move up or down for a snake or ladder
+    square += board[square]
+    // roll the dice
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    // move by the rolled amount
+    square += diceRoll
+} while square < finalSquare*/
+
+
+
+
+
+
+print("Game over!")
