a5277f76d83698d836ba7aef8e035cea73ab8df7
Afrah Omar <afrahomar@MacBook-Pro-Afrah.local>
2021-10-13 17:26:47 +0300

    hello

--- Lab05 .playground/Contents.swift
+++ Lab05 .playground/Contents.swift
@@ -9,38 +9,15 @@ board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
 var square = 0
 var diceRoll = 0
 
-// Version with While Loop
 while square < finalSquare {
-    // roll the dice
+
     diceRoll = Int.random(in: 1...6)
     print("The Number of dice is \(diceRoll)")
     
-    // move by the rolled amount
+ 
     square += diceRoll
     print("My current square is \(square)")
     
     if square < board.count {
-        // if we're still on the board, move up or down for a snake or a ladder
-        square += board[square]
-        print("I move to \(square)")
-    }
-}
-
-// Version with Repeat Loop
-/*repeat {
-    // move up or down for a snake or ladder
-    square += board[square]
-    // roll the dice
-    diceRoll += 1
-    if diceRoll == 7 { diceRoll = 1 }
-    // move by the rolled amount
-    square += diceRoll
-} while square < finalSquare*/
-
-
-
-
-
-
 print("Game over!")
 
