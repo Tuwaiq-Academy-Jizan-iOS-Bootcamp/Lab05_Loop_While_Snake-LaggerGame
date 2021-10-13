213ee366ff1ce3480a7ef29799cb438191f5eb32
yasir <donyasser4@gmail.com>
2021-10-13 14:30:10 +0300

    my lab for today

---
+++ squares.playground/Contents.swift
@@ -0,0 +1,4 @@
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+print("squares number\(array[0...5]) use the legger to 17")
+print("squares number\(array[16...18]) avoid the snake")
+print("squares number\(array[19...24]) the end")
